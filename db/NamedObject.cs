e44b47badb69196d3cad94530cec00bda7570c62
Philip Bailey <philip@northarrowresearch.com>
2017-02-01 15:27:24 -0800

    cleaning up code

--- db/NamedObject.cs
+++ db/NamedObject.cs
@@ -4,26 +4,18 @@ namespace naru.db
 {
     public class NamedObject
     {
-        private long m_nID;
-        private String m_sName;
+        public long ID { get; internal set; }
+        public String Name { get; internal set; }
 
         public NamedObject(long nID, String sName)
         {
-            m_nID = nID;
-            m_sName = sName;
+            ID = nID;
+            Name = sName;
         }
 
         public override string ToString()
         {
-            return m_sName;
-        }
-
-        public long ID
-        {
-            get
-            {
-                return m_nID;
-            }
+            return Name;
         }
     }
 }
