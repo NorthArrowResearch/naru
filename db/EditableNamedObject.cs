d142cba51bf3089ac583fa6f1f426923c6a3e908
Philip Bailey <philip@northarrowresearch.com>
2017-02-05 13:40:04 -0800

    moving editable DB object to its own class

---
+++ db/EditableNamedObject.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace naru.db
+{
+    public enum DBState
+    {
+        New,
+        Edited,
+        Unchanged
+    }
+
+    public class EditableNamedObject : NamedObject
+    {
+        public DBState m_eState;
+
+        public DBState State
+        {
+            get { return m_eState; }
+            protected set
+            {
+                if (m_eState != DBState.New)
+                    m_eState = DBState.Edited;
+            }
+        }
+
+        public EditableNamedObject(long nID, String sName)
+            : base(nID, sName)
+        {
+            if (nID > 0)
+                State = DBState.Unchanged;
+            else
+                State = DBState.New;
+        }
+
+        public new string Name
+        {
+            get { return m_sName; }
+            set
+            {
+                base.Name = value;
+                if (m_eState != DBState.New)
+                    m_eState = DBState.Edited;
+            }
+        }
+    }
+}
--- db/NamedObject.cs
+++ db/NamedObject.cs
@@ -2,49 +2,21 @@
 
 namespace naru.db
 {
-    public enum DBState
-    {
-        New,
-        Edited,
-        Unchanged
-    }
-
     public class NamedObject
     {
         public long ID { get; internal set; }
         public String m_sName { get; internal set; }
-
-        public DBState m_eState;
         
-        public DBState State
-        {
-            get { return m_eState; }
-            protected set
-            {
-                if (State != DBState.New)
-                    State = DBState.Edited;
-            }
-        }
-
         public string Name
         {
             get { return m_sName; }
-            set
-            {
-                m_sName = value;
-                State = DBState.Edited;
-            }
+            set { m_sName = value; }
         }
 
         public NamedObject(long nID, String sName)
         {
             ID = nID;
             m_sName = sName;
-
-            if (nID > 0)
-                State = DBState.Unchanged;
-            else
-                State = DBState.New;
         }
 
         public override string ToString()
