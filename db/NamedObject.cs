40e0a8410d2c295772c36687c91440c1580130a6
Philip Bailey <philip@northarrowresearch.com>
2017-02-04 20:01:34 -0800

    tracking status with named object

--- db/NamedObject.cs
+++ db/NamedObject.cs
@@ -2,15 +2,49 @@
 
 namespace naru.db
 {
+    public enum DBState
+    {
+        New,
+        Edited,
+        Unchanged
+    }
+
     public class NamedObject
     {
         public long ID { get; internal set; }
-        public String Name { get; internal set; }
+        public String m_sName { get; internal set; }
+
+        public DBState m_eState;
+        
+        public DBState State
+        {
+            get { return m_eState; }
+            protected set
+            {
+                if (State != DBState.New)
+                    State = DBState.Edited;
+            }
+        }
+
+        public string Name
+        {
+            get { return m_sName; }
+            set
+            {
+                m_sName = value;
+                State = DBState.Edited;
+            }
+        }
 
         public NamedObject(long nID, String sName)
         {
             ID = nID;
-            Name = sName;
+            m_sName = sName;
+
+            if (nID > 0)
+                State = DBState.Unchanged;
+            else
+                State = DBState.New;
         }
 
         public override string ToString()
