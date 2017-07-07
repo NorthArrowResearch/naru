472d2df0b27ab4f9d0faa4604feddf65bf545f06
Philip Bailey <philip@northarrowresearch.com>
2017-07-06 20:52:11 -0700

    removing unused code in multi key cross tab

--- db/sqlite/CrossTabMultiColumn.cs
+++ db/sqlite/CrossTabMultiColumn.cs
@@ -10,18 +10,15 @@ namespace naru.db.sqlite
 {
     public class CrossTabMultiColumn : CrossTab
     {
-        public static DataTable CreateCrossTab(string sDBCon, List<Tuple<string, string>> KeyColumns, string sqlCols, string sqlRows, string sqlContents)
+        public static DataTable CreateCrossTab(string sDBCon, List<Tuple<string, string>> KeyColumns, string sqlCols, string sqlContents)
         {
             DataTable dt = new DataTable();
-
-            //Dictionary<string, int> keyColumnIndices = new Dictionary<string, int>();
 
             foreach (Tuple<string, string> aCol in KeyColumns)
             {
                 // Add the key column of labels
                 dt.Columns.Add(aCol.Item1, Type.GetType("System.String"));
                 dt.Columns[dt.Columns.Count - 1].Caption = aCol.Item2;
-                //keyColumnIndices[aCol.Item1] = dt.Columns.Count - 1;
             }
 
             // Add the remaining columns and build a dictionary of the IDs to the column index
@@ -32,36 +29,7 @@ namespace naru.db.sqlite
             PopulateCrossTab(sDBCon, sqlContents, ref dt, KeyColumns, dColLookup);
 
             return dt;
-
         }
-
-        //private static void AppendRows(string sDBCon, string sqlRows, ref DataTable dt, List<Tuple<string, string>> Keycolumns, out Dictionary<long, object> dRows)
-        //{
-        //    dRows = new Dictionary<long, object>();
-        //    using (SQLiteConnection dbCon = new SQLiteConnection(sDBCon))
-        //    {
-        //        dbCon.Open();
-        //        SQLiteCommand dbCom = new SQLiteCommand(sqlRows, dbCon);
-        //        System.Diagnostics.Debug.Print("Crosstab row SQL: {0}", sqlRows);
-        //        SQLiteDataReader dbRead = dbCom.ExecuteReader();
-        //        while (dbRead.Read())
-        //        {
-        //            for (int i = 0; i < Keycolumns.Count; i++)
-        //            {
-        //                long nIDValue = dbRead.GetInt64(dbRead.GetOrdinal(Keycolumns[i].Item1));
-        //                if (i < Keycolumns.Count - 1)
-        //                    dRows
-        //            }
-
-
-        //            long nRowID = dbRead.GetInt64(0);
-        //            DataRow row = dt.NewRow();
-        //            row.SetField<string>(0, naru.db.sqlite.SQLiteHelpers.GetSafeValueStr(ref dbRead, 1));
-        //            dRows[nRowID] = dt.Rows.Count;
-        //            dt.Rows.Add(row);
-        //        }
-        //    }
-        //}
 
         private static void PopulateCrossTab(string sDBCon, string sqlContents, ref DataTable dt, List<Tuple<string, string>> Keycolumns, Dictionary<long, int> dCols)
         {
