9a81c9ef63892aeb4292c96c1b8e1e222243e9ef
Philip Bailey <philip@northarrowresearch.com>
2017-07-12 11:30:13 -0700

    switching to single database definition structure file

--- db/sqlite/DBManager.cs
+++ db/sqlite/DBManager.cs
@@ -13,9 +13,7 @@ namespace naru.db.sqlite
         public System.IO.FileInfo FilePath { get; internal set; }
         public string SQLVersion { get; internal set; }
         public int MinimumSupportedVersion { get; internal set; }
-
         private string DatabaseStructureSQLFileName { get; set; }
-        private string DatabaseContentsWildcardPattern { get; set; }
 
         public override string ToString()
         {
@@ -70,8 +68,7 @@ namespace naru.db.sqlite
         /// 
         /// Each of the data SQL files must have the corresponding database table name after this DataWildcard.
         /// For example if the wildcard is "_data_" an example file name might be mytool_data_MyTable.sql</remarks>
-        public DBManager(string sFilePath, string sqlVersionQuery, int nMinSupporterVersion
-            , string sDBStructureSQL, string sDBDataWildcard)
+        public DBManager(string sFilePath, string sqlVersionQuery, int nMinSupporterVersion, string sDBStructureSQL)
         {
             if (string.IsNullOrEmpty(sFilePath))
                 throw new Exception("Empty database file path");
@@ -79,7 +76,6 @@ namespace naru.db.sqlite
             SQLVersion = sqlVersionQuery;
             FilePath = new System.IO.FileInfo(sFilePath);
             DatabaseStructureSQLFileName = sDBStructureSQL;
-            DatabaseContentsWildcardPattern = sDBDataWildcard;
         }
 
         public int GetDBVersion()
@@ -119,122 +115,33 @@ namespace naru.db.sqlite
         /// </summary>
         public void CreateDatabase()
         {
-            string sqlStructureSQL = string.Empty;
-            List<string> sqlContentsSQL = null;
-
             try
             {
                 // Load the structure and data SQL first so any problems occur before DB created on disk
-                LoadDBStructureStatements(out sqlStructureSQL);
-                LoadDataStatements(out sqlContentsSQL);
+                string structurePath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), DatabaseStructureSQLFileName);
+                string sqlDBStructure = System.IO.File.ReadAllText(structurePath);
 
                 // Create the empty SQLite database file
-                CreateEmptyDatabase();
+                SQLiteConnection.CreateFile(FilePath.FullName);
 
-                // Build the DB structure and populate lookup tables
-                PopulateEmptyDatabase(sqlStructureSQL, sqlContentsSQL);
+                // Loop over each table that has lookup data defined and insert it.
+                // Ensure referential integrity is off to make this insensitive to the order of the files 
+                using (SQLiteConnection dbCon = new SQLiteConnection(ConnectionString))
+                {
+                    dbCon.Open();
+                    SQLiteCommand dbCom = new SQLiteCommand(sqlDBStructure, dbCon);
+                    dbCom.ExecuteNonQuery();
+
+                    // Turn referential integrity back on
+                    dbCom = new SQLiteCommand("PRAGMA foreign_keys = on;", dbCon);
+                    dbCom.ExecuteNonQuery();
+                }
             }
             catch (Exception ex)
             {
                 ex.Data["Database File Path"] = FilePath.FullName;
                 ex.Data["Database Structure File Path"] = DatabaseStructureFilePath;
-                ex.Data["Lookup Data Wildard"] = DatabaseContentsWildcardPattern;
                 throw;
-            }
-        }
-
-        /// <summary>
-        /// Open the text file that contains the structure SQL statements and reads them into a string
-        /// </summary>
-        /// <param name="sqlDBStructure"></param>
-        private void LoadDBStructureStatements(out string sqlDBStructure)
-        {
-            sqlDBStructure = System.IO.File.ReadAllText(DatabaseStructureFilePath);
-
-            if (string.IsNullOrEmpty(sqlDBStructure))
-                throw new Exception("The database structure file is empty.");
-        }
-
-        /// <summary>
-        /// Creates the actual SQLite database file on disk
-        /// </summary>
-        /// <remarks>Note that the database will have no tables, views or data etc.</remarks>
-        private void CreateEmptyDatabase()
-        {
-            // Verify that the output new database file does not already exist, then create it
-            if (FilePath.Exists)
-                throw new Exception("The database file path already exists.");
-
-            SQLiteConnection.CreateFile(FilePath.FullName);
-        }
-
-        /// <summary>
-        /// Find all text files containing lookup data and load the SQL commands in each file into a list
-        /// </summary>
-        /// <param name="sqlDataStatements">List of lookup data insert commands. One item in list per database lookup table.</param>
-        private void LoadDataStatements(out List<string> sqlDataStatements)
-        {
-            sqlDataStatements = new List<string>();
-
-            // Find all SQL files that insert lookup data into the database. These should be the data insert queries (e.g. Workbench_data_USGS_Gages.sql).
-            string sSearchPath = string.Format("*{0}*.sql", DatabaseContentsWildcardPattern);
-            foreach (string sFilePath in System.IO.Directory.GetFiles(System.IO.Path.GetDirectoryName(DatabaseStructureFilePath), sSearchPath))
-            {
-                // Determine the data table from the filename that follows the keyword _data_ and preceeds the file suffix.
-                Regex re = new Regex(string.Format(".*_data_(.*).sql", DatabaseContentsWildcardPattern));
-                Match tableNameMatch = re.Match(sFilePath);
-                string sTableName = tableNameMatch.Groups[1].ToString();
-
-                // Replace the table placeholder text with the actual database table name
-                string sSQLStatements = System.IO.File.ReadAllText(sFilePath);
-                sSQLStatements = sSQLStatements.Replace("INSERT INTO table", string.Format("INSERT INTO {0}", sTableName));
-                sqlDataStatements.Add(sSQLStatements);
-            }
-        }
-
-        /// <summary>
-        /// Takes and empty SQLite database file and executes the commands to create tables, views and fill lookup data tables
-        /// </summary>
-        /// <param name="sqlStructure">All the SQL commands to create the database structure. Separated by semicolon</param>
-        /// <param name="sqlData">List of SQL commands to populate lookup tables with data. Each list item is a separate lookup table</param>
-        private void PopulateEmptyDatabase(string sqlStructure, List<string> sqlData)
-        {
-            // Loop over each table that has lookup data defined and insert it.
-            // Ensure referential integrity is off to make this insensitive to the order of the files 
-            using (SQLiteConnection dbCon = new SQLiteConnection(ConnectionString))
-            {
-                dbCon.Open();
-                SQLiteCommand dbCom = new SQLiteCommand("PRAGMA foreign_keys = off;", dbCon);
-                dbCom.ExecuteNonQuery();
-
-                // Not strictly needed because errors are irrecoverable, but speeds up the operation.
-                SQLiteTransaction dbTrans = dbCon.BeginTransaction();
-
-                try
-                {
-                    // Build the database structure
-                    dbCom = new SQLiteCommand(sqlStructure, dbTrans.Connection, dbTrans);
-                    dbCom.ExecuteNonQuery();
-
-                    // Populate each lookup table with data
-                    foreach (string sqlDataCommand in sqlData)
-                    {
-                        dbCom = new SQLiteCommand(sqlDataCommand, dbTrans.Connection, dbTrans);
-                        dbCom.ExecuteNonQuery();
-                    }
-
-                    // Turn referential integrity back on
-                    dbCom = new SQLiteCommand("PRAGMA foreign_keys = on;", dbTrans.Connection, dbTrans);
-                    dbCom.ExecuteNonQuery();
-
-                    dbTrans.Commit();
-                }
-                catch (Exception ex)
-                {
-                    dbTrans.Rollback();
-                    ex.Data["File Path"] = FilePath.FullName;
-                    throw;
-                }
             }
         }
 
