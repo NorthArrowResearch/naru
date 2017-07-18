574b75fd26252f2ecf4c72fdbdf30da06b0bd6cb
Philip Bailey <philip@northarrowresearch.com>
2017-07-18 16:50:13 -0700

    progress bar percent calculation

---
+++ ui/ProgressBar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naru.ui
+{
+    public class ProgressBar
+    {
+        public static int ProgressPercent(int nVisitCounter, int TotalNumberVisits)
+        {
+            if (TotalNumberVisits == 0)
+                return 0;
+            else
+            {
+                if (nVisitCounter == 0)
+                    return 0;
+                else
+                    if (nVisitCounter == TotalNumberVisits)
+                    return 100;
+                else
+                    return (int)(100.0 * (double)nVisitCounter / (double)TotalNumberVisits);
+            }
+        }
+    }
+}
