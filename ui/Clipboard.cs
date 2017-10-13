8871fc759375cb3a2b6a26e2667b4e265828cf15
Philip Bailey <philip@northarrowresearch.com>
2017-10-13 15:29:49 -0700

    copy to clipboard

--- ui/Clipboard.cs
+++ ui/Clipboard.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
-namespace PipSuite.naru.ui
+namespace naru.ui
 {
-    public class Clipboard
+    public struct Clipboard
     {
-        public static void SetText(string text)
+        /// <summary>
+        /// Copy text to the clipboard
+        /// </summary>
+        /// <param name="sText">The data to place on the Clipboard.</param>
+        /// <param name="bKeepAfterAppExits">true if you want data to remain on the Clipboard after this application exits; otherwise, false.</param>
+        /// <param name="nRetries">The number of times to attempt placing the data on the Clipboard.</param>
+        /// <param name="nDelay">The number of milliseconds to pause between attempts.</param>
+        public static void SetText(string sText, bool bKeepAfterAppExits = false, int retryTimes = 5, int retryDelay = 200)
         {
-            System.Windows.Forms.Clipboard.SetData(DataFormats.Text, (Object)text);
+            System.Windows.Forms.Clipboard.SetDataObject(sText, bKeepAfterAppExits, retryTimes, retryDelay);
         }
     }
 }
