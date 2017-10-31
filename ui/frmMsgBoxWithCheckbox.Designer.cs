18513b47cd7f1e001d1d56e85510671cb7a0da61
Philip Bailey <philip@northarrowresearch.com>
2017-10-31 16:57:30 -0700

    messagebox with checkbox

---
+++ ui/frmMsgBoxWithCheckbox.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace naru.ui
+{
+    partial class frmMsgBoxWithCheckbox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.chkReminder = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdCancel.Location = new System.Drawing.Point(479, 86);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(75, 23);
+            this.cmdCancel.TabIndex = 0;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.cmdOK.Location = new System.Drawing.Point(398, 86);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(75, 23);
+            this.cmdOK.TabIndex = 1;
+            this.cmdOK.Text = "OK";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.Location = new System.Drawing.Point(12, 9);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(542, 64);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "label1";
+            // 
+            // chkReminder
+            // 
+            this.chkReminder.AutoSize = true;
+            this.chkReminder.Location = new System.Drawing.Point(15, 76);
+            this.chkReminder.Name = "chkReminder";
+            this.chkReminder.Size = new System.Drawing.Size(182, 17);
+            this.chkReminder.TabIndex = 3;
+            this.chkReminder.Text = "Do not show this message again.";
+            this.chkReminder.UseVisualStyleBackColor = true;
+            // 
+            // frmMsgBoxWithCheckbox
+            // 
+            this.AcceptButton = this.cmdOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cmdCancel;
+            this.ClientSize = new System.Drawing.Size(566, 121);
+            this.Controls.Add(this.chkReminder);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.cmdOK);
+            this.Controls.Add(this.cmdCancel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMsgBoxWithCheckbox";
+            this.Text = "frmMsgBoxWithCheckbox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.Label lblMessage;
+        public System.Windows.Forms.CheckBox chkReminder;
+    }
+}
---
+++ ui/frmMsgBoxWithCheckbox.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace naru.ui
+{
+    public partial class frmMsgBoxWithCheckbox : Form
+    {
+        public frmMsgBoxWithCheckbox(string sMessage)
+        {
+            InitializeComponent();
+            lblMessage.Text = sMessage;
+        }
+    }
+}
