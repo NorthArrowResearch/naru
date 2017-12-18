13636724d51236fb97c3ace368bb9a27e91e739c
Philip Bailey <philip@northarrowresearch.com>
2017-12-17 21:05:13 -0800

    clipboard

---
+++ ui/Clipboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PipSuite.naru.ui
+{
+    public class Clipboard
+    {
+        public static void SetText(string text)
+        {
+            System.Windows.Forms.Clipboard.SetData(DataFormats.Text, (Object)text);
+        }
+    }
+}
