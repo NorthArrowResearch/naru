5dec4fa9c6a96c4f2fae508836a8dc2164111d40
Philip Bailey <philip@northarrowresearch.com>
2017-12-18 14:33:17 -0800

    menu button

---
+++ ui/MenuButton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace naru.ui
+{
+    public class MenuButton : Button
+    {
+        // https://stackoverflow.com/questions/10803184/windows-forms-button-with-drop-down-menu
+
+        [DefaultValue(null)]
+        public ContextMenuStrip Menu { get; set; }
+
+        [DefaultValue(false)]
+        public bool ShowMenuUnderCursor { get; set; }
+
+        protected override void OnMouseDown(MouseEventArgs mevent)
+        {
+            base.OnMouseDown(mevent);
+
+            if (Menu != null && mevent.Button == MouseButtons.Left)
+            {
+                Point menuLocation;
+
+                if (ShowMenuUnderCursor)
+                {
+                    menuLocation = mevent.Location;
+                }
+                else
+                {
+                    menuLocation = new Point(0, Height);
+                }
+
+                Menu.Show(this, menuLocation);
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+
+            if (Menu != null)
+            {
+                int arrowX = ClientRectangle.Width - 14;
+                int arrowY = ClientRectangle.Height / 2 - 1;
+
+                Brush brush = Enabled ? SystemBrushes.ControlText : SystemBrushes.ButtonShadow;
+                Point[] arrows = new Point[] { new Point(arrowX, arrowY), new Point(arrowX + 7, arrowY), new Point(arrowX + 3, arrowY + 4) };
+                pevent.Graphics.FillPolygon(brush, arrows);
+            }
+        }
+    }
+}
