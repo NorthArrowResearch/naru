10efdb332c737dbb409992d25deb661a13364a7e
Philip Bailey <philip@northarrowresearch.com>
2019-12-20 15:41:44 -0800

    removing unneeded using statements

--- db/sqlite/DBCon.cs
+++ db/sqlite/DBCon.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Data.SQLite;
-using System.Xml;
+﻿using System.Collections.Generic;
 
 namespace naru.db.sqlite
 {
